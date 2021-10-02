f9838848332988891cd8b450315af1a4bdc2a3c4
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-02 22:24:31 +0500

    capstone

---
+++ src/components/json/Json.js
@@ -0,0 +1,224 @@
+// import logo1 from './images/sam2.jpg';
+// import logo2 from './images/sam3.jpg';
+// import logo3 from './images/sam4.png';
+// import logo4 from './images/sam5.jpg';
+// import logo5 from './images/p00.jpg';
+// import logo6 from './images/p1.jpg';
+// import logo7 from './images/p2.png';
+// import logo8 from './images/p3.jpg';
+// import logo9 from './images/op11.jpg';
+// import logo10 from './images/op12.jpg';
+// import logo11 from './images/op13.jpg';
+// import logo12 from './images/op14.jpg';
+// import logo13 from './images/hu11.png';
+// import logo14 from './images/hu12.jpg';
+// import logo15 from './images/hu13.jpg';
+// import logo16 from './images/hu14.jpg';
+
+
+
+const PhoneData =
+[
+   {"category": "Samsung",
+   "id":1,
+   "model": "Galaxy Z flip3",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam2.jpg'
+​},
+{
+"category": "Samsung",
+   "id":2,
+   "model": "Galaxy A52",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture": './images/sam3.jpg'
+​},
+{"category": "Samsung",
+   "id":3,
+   "model": "Galaxy F62",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+​},
+{"category": "Samsung",
+   "id":4,
+   "model": "Galaxy S21",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+​},
+{
+   "category": "Apple",
+   "id":5,
+   "model": "iPhone XS",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+​},
+{
+   "category": "Apple",
+   "id":6,
+   "model": "iPhone 11 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+​
+​
+},
+{
+   "category": "Apple",
+   "id":7,
+   "model": "iPhone 12 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+},
+​
+{
+   "category": "Apple",
+   "id":8,
+   "model": "iPhone 13 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+​
+},
+{
+   "category": "Oppo",
+   "id":9,
+   "model": "OPPO A16",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+},
+{
+   "category": "Oppo",
+   "id":10,
+   "model": "Reno6 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+ 
+},
+{
+   "category": "Oppo",
+   "id":11,
+   "model": "OPPO F19 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'    
+ 
+​
+},
+{
+   "category": "Oppo",
+   "id":12,
+   "model": "OPPO Find X2 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+   
+},
+{
+   "category": "Huawei",
+   "id":13,
+   "model": "HUAWEI Y9a",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg' 
+  
+},
+{
+   "category": "Huawei",
+   "id":14,
+   "model": "HUAWEI nova 7i",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'  
+ 
+},
+{
+   "category": "Huawei",
+   "id":15,
+   "model": "'HUAWEI Y9s",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+  
+},
+{
+   "category": "Huawei",
+   "id":16,
+   "model": "HUAWEI Mate 30 Pro",
+  "body": "",
+  "display": "",
+  "platform":"",
+  "memory": "",
+  "battery":"",
+  "price":"",
+  "picture":'./images/sam3.jpg'
+   
+}
+]
+export default PhoneData;
